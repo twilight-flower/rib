3d3c4a947ee541ffc391016143429aeeb48d6ab5
Alyssa <alyssa@mountainlair.net>
2025-12-28 16:54:26 -0600

    implement arg-parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::path::PathBuf;
+
+use argh::FromArgs;
+
+//////////////
+//   Args   //
+//////////////
+
+#[derive(FromArgs)]
+#[argh(subcommand)]
+enum Subcommand {} // Placeholder
+
+#[derive(FromArgs)]
+/// Minimalist EPUB reader.
+struct Args {
+    #[argh(subcommand)]
+    subcommand: Option<Subcommand>,
+    #[argh(positional)]
+    /// epub path to open
+    path: PathBuf, // Maybe switch to vec to allow opening multiple epubs at once?
+}
+
+//////////////
+//   Main   //
+//////////////
+
 fn main() {
-    
+    let args: Args = argh::from_env();
+    println!("Attempting to open {}.", args.path.display());
 }
