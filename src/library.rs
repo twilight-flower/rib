278d958743d4133ccbf03a5114a5fc4441348ebf
Alyssa <alyssa@mountainlair.net>
2026-01-01 18:27:45 -0600

    track raw resource paths per book

--- src/library.rs
+++ src/library.rs
@@ -19,18 +19,28 @@ use crate::{
 };
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct LibraryBookSpineItem {
+    pub path: PathBuf,
+    pub linear: bool,
+    // properties can go here later, but ignore them for now
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct LibraryBookRenditionInfo {
     pub dir_path_from_library_root: PathBuf,
-    pub file_path_from_library_root: PathBuf,
+    pub default_file_path_from_library_root: PathBuf,
     pub bytes: u64,
 }
 
 impl LibraryBookRenditionInfo {
     pub fn open_in_browser(&self, library_path: &PathBuf, browser: &Option<String>) {
-        let path_to_open = library_path
-            .join(&self.file_path_from_library_root)
+        let path_to_canonicalize = library_path.join(&self.default_file_path_from_library_root);
+        let path_to_open = path_to_canonicalize
             .canonicalize() // To help cross-platform compatibility, hopefully
-            .expect("Unable to canonicalize book rendition path to open.");
+            .expect(&format!(
+                "Unable to canonicalize book rendition path {} to open.",
+                path_to_canonicalize.display()
+            ));
         browser::open(&path_to_open, browser);
     }
 }
@@ -54,6 +64,10 @@ pub struct LibraryBookInfo {
     )]
     pub last_opened_time: DateTime<Utc>,
 
+    // Contents
+    pub raw_spine_items: Vec<LibraryBookSpineItem>,
+    pub raw_nonspine_resource_paths: Vec<PathBuf>,
+
     // Renditions
     pub raw_rendition: LibraryBookRenditionInfo,
     // pub renditions: Vec<LibraryBookRenditionInfo>,
@@ -70,6 +84,7 @@ impl LibraryBookInfo {
         let path_from_library_root = library.get_internal_path_from_id(&epub_id);
         let raw_dir_path_from_library_root = path_from_library_root.join("raw");
         let raw_dir = library.library_path.join(&raw_dir_path_from_library_root);
+
         for (id, resource) in epub.resources.clone() {
             let resource_path = raw_dir.join(resource.path);
             match resource_path.starts_with(&raw_dir) {
@@ -92,38 +107,51 @@ impl LibraryBookInfo {
             }
         }
 
-        let first_linear_spine_item_idref = &epub
-            .spine
-            .iter()
-            .find(|item| item.linear)
-            .expect("Ill-formed EPUB: no linear spine items.")
-            .idref;
-        let first_linear_spine_item_path = standardize_pathbuf_separators(&epub
-            .resources
-            .get(first_linear_spine_item_idref)
-            .expect(
-                "Internal error: EPUB library failed to get resource for id listed in its spine.",
-            )
-            .path);
-
         println!(
             "Dumped raw contents of {} to {}.",
             epub_path.display(),
             raw_dir.display()
         );
 
-        let raw_rendition_dir_size = get_dir_size(&raw_dir);
+        let raw_spine_items = epub.spine.iter().map(|spine_item| LibraryBookSpineItem {
+            path: standardize_pathbuf_separators(&raw_dir_path_from_library_root.join(&epub.resources.get(&spine_item.idref).expect("Internal error: EPUB library failed to get resource for id listed in its spine.").path)),
+            linear: spine_item.linear,
+        }).collect_vec();
+        let raw_nonspine_resource_paths = epub
+            .resources
+            .values()
+            .filter_map(|resource| {
+                let resource_path = raw_dir_path_from_library_root.join(&resource.path);
+                match raw_spine_items
+                    .iter()
+                    .any(|spine_item| spine_item.path == resource_path)
+                {
+                    true => None,
+                    false => Some(standardize_pathbuf_separators(&resource_path)),
+                }
+            })
+            .collect_vec();
+
+        let first_linear_raw_spine_item_path = standardize_pathbuf_separators(
+            &raw_spine_items
+                .iter()
+                .find(|item| item.linear)
+                .expect("Ill-formed EPUB: no linear spine items.")
+                .path,
+        );
+
         Self {
             id: epub_id,
             title: epub.get_title().expect("Ill-formed EPUB: no title."),
             path_from_library_root,
             added_time: request_time,
             last_opened_time: request_time,
+            raw_spine_items,
+            raw_nonspine_resource_paths,
             raw_rendition: LibraryBookRenditionInfo {
-                file_path_from_library_root: raw_dir_path_from_library_root
-                    .join(first_linear_spine_item_path),
+                default_file_path_from_library_root: first_linear_raw_spine_item_path,
                 dir_path_from_library_root: raw_dir_path_from_library_root,
-                bytes: raw_rendition_dir_size,
+                bytes: get_dir_size(&raw_dir),
             },
         }
     }
